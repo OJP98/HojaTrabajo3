ee41f9d5a8af3b5ac7de29bd07076e845aa7fa15
OJP98 <ojuarez_p@hotmail.com>
2018-02-02 12:07:28 -0600

    Repositorio creado, primera clase implementada

---
+++ HojaTrabajo3/src/hojatrabajo3/HojaTrabajo3.java
@@ -0,0 +1,17 @@
+/**
+ * Clase que realiza las interacciones con el usuario.
+ * @author: Oscar Juarez - 17315 / Guillermo Sandoval - 17577
+ * @version: 9/02/17
+ */
+package hojatrabajo3;
+
+public class HojaTrabajo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
